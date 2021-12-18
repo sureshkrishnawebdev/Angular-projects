d2e3d369601c714916121ec6c9cff69ae29c71f8
sureshraxz <sureshraxz@gmail.comm>
2021-12-18 22:06:44 +0530

    step 3.4.1 creating methods

---
+++ fitness-app/src/auth/shared/services/auth/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+@Injectable()
+export class AuthService {
+
+    constructor(
+        private af:AngularFireAuth,        
+    ) { }
+
+    createUser( email: string, password: string ): Promise<any> {
+        return this.af
+          .createUserWithEmailAndPassword( email, password);
+    }
+
+    loginUser(  email: string, password: string ): Promise<any> {
+        return this.af
+            .signInWithEmailAndPassword( email, password);
+    }
+
+}
