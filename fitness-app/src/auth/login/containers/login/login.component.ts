719db125bb2e92d74fd4349f907bf655fec04bcb
sureshraxz <sureshraxz@gmail.comm>
2021-12-19 00:21:29 +0530

    step 3.4.3. Consuming the service

--- fitness-app/src/auth/login/containers/login/login.component.ts
+++ fitness-app/src/auth/login/containers/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/auth/shared/services/auth/auth.service';
 
 @Component({
     selector:'app-w3hTech-login',
@@ -8,11 +10,15 @@ import { FormGroup } from '@angular/forms';
             <app-w3hTech-auth-form
                 (submitted)="onLoginUser($event)">
 
-                <h1> Login </h1>
+                <h1> Login </h1>               
 
                 <button mat-raised-button> Login </button>
 
                 <a routerLink="/auth/register"> Not Registerd ?</a>
+               
+                <label class="error"  *ngIf="error">
+                    {{ error }}
+                </label>
 
             </app-w3hTech-auth-form>
         </div>
@@ -20,8 +26,24 @@ import { FormGroup } from '@angular/forms';
 })
 export class LoginComponent {
 
-onLoginUser( event: FormGroup ): void{
-    console.log(`inside login `); console.log(event.value);
+    error!: string;
+
+    constructor( 
+        private authService: AuthService,
+        private router: Router,
+     ) { }
+
+async onLoginUser( event: FormGroup ): Promise<any>{
+    const { email, password } = event.value;
+    try{
+        await this.authService.loginUser(email, password);
+
+        // success
+        this.router.navigate(['/']);
+
+    } catch( err ) {
+        this.error = err.message;   //specific to firebase
+    }
 }
 
 }
--- fitness-app/src/auth/register/containers/register/register.component.ts
+++ fitness-app/src/auth/register/containers/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/auth/shared/services/auth/auth.service';
 
 @Component({
     selector:'app-w3hTech-register',
@@ -13,6 +15,10 @@ import { FormGroup } from '@angular/forms';
             <button mat-raised-button color="button-css"> Create account </button>
         
             <a routerLink="/auth/login"> Already have an account ? </a>
+
+            <label class="error"  *ngIf="error">
+            {{ error }}
+            </label>
             
         </app-w3hTech-auth-form>
             
@@ -21,8 +27,24 @@ import { FormGroup } from '@angular/forms';
 })
 export class RegisterComponent {
 
-onRegisterUser( event: FormGroup ): void{
-    console.log(`isnide register`); console.log(event.value);
+    error!: string;
+
+    constructor( 
+        private authService: AuthService,
+        private router:Router,
+     ) { }
+
+async onRegisterUser( event: FormGroup ): Promise<any>{
+    const { email, password } = event.value;
+    try{
+        await this.authService.createUser(email, password);
+
+        // success
+        this.router.navigate(['/']);
+
+    } catch( err ) {
+        this.error = err.message;   //specific to firebase
+    }
+}
 }
     
-}
