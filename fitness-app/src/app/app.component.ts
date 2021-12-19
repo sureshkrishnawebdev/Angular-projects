c3c65c287b92e352ae8ac27cc411f99449c7268b
sureshraxz <sureshraxz@gmail.comm>
2021-12-19 23:08:18 +0530

    step 4.3. Invoking the store and setting value

--- fitness-app/src/app/app.component.ts
+++ fitness-app/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { Observable, Subscription } from 'rxjs';
+
+import { AuthService, User } from 'src/auth/shared/services/auth/auth.service';
+
+import { Store } from 'store';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'fitness-app';
+export class AppComponent implements OnInit, OnDestroy{
+
+  user$!: Observable<User>;
+  subscription!: Subscription;
+
+  constructor( 
+    private authService: AuthService,
+    private store: Store,    
+  ) { }
+
+  ngOnInit(): void {
+
+    // Invoking the auth observable - to initate dataflow into our store
+      this.subscription = this.authService.auth$.subscribe();
+
+    // Consuming value from store
+      this.user$ = this.store.select<User>('user');
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
 }
--- fitness-app/src/auth/shared/services/auth/auth.service.ts
+++ fitness-app/src/auth/shared/services/auth/auth.service.ts
@@ -1,12 +1,49 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 
+// store
+import { Store } from 'store';
+
+// rxjs
+import { tap } from 'rxjs/operators';
+
+export interface User {
+    email: string,
+    uid: string,
+    authenticated: boolean,
+}
+
 @Injectable()
 export class AuthService {
 
     constructor(
-        private af:AngularFireAuth,        
+        private af: AngularFireAuth,   
+        private store: Store,     
     ) { }
+
+    // Store 
+    auth$ = this.af.authState
+        .pipe(
+            tap( next => {
+                // fall back - when user is not logged in
+                    if( ! next ) {
+                        this.store.set( 'user', null );
+                        return;
+                    }
+
+                // success - when user is logged in
+                    const user: User = {
+                        email: next.email as string,
+                        uid: next.uid,
+                        authenticated: true,
+                    };
+
+                    this.store.set( 'user', user );
+            })
+        )
+
+
+    // Firebase methods
 
     createUser( email: string, password: string ): Promise<any> {
         return this.af
